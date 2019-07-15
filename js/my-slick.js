d62b588b78e317b2725216735066ab8e3a92badc
Dmitry Komlev <komlev.mitya2011@gmail.com>
2019-07-15 17:33:39 +0300

    add files

---
+++ js/my-slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    // autoplay: true,
+    pauseOnDotsHover: true,
+    arrows: false,
+    speed: 1000,
+    appendDots: $('header')
+  });
+});
